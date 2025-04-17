520dbae9345d6a3bb5d5b664f65a2c4eae9666e3
Lam Nguyen <topovn0908@gmail.com>
2025-04-17 17:13:34 +1000

    Initial commit

---
+++ sketchybar/items/weather.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+COLOR="$ORANGE"
+
+sketchybar \
+  --add item weather left \
+  --set weather \
+  update_freq=120 \
+  icon.font="$FONT:Bold:15.0" \
+  background.padding_left=0 \
+  background.padding_right=10 \
+  icon.color="$COLOR" \
+  icon.padding_left=10 \
+  label.color="$COLOR" \
+  label.padding_right=10 \
+  background.height=26 \
+  background.corner_radius="$CORNER_RADIUS" \
+  background.padding_right=5 \
+  background.border_width="$BORDER_WIDTH" \
+  background.border_color="$COLOR" \
+  background.color="$BAR_COLOR" \
+  background.drawing=on \
+  script="$PLUGIN_DIR/weather.sh"
