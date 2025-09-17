4ca6439351cf0ea816f1c4413796124a6e471e1c
Lam Nguyen <topovn0908@gmail.com>
2025-09-17 19:45:00 +1000

    update network sketchybar cfg

--- sketchybar/plugins/network.sh
+++ sketchybar/plugins/network.sh
@@ -3,6 +3,9 @@
 # # Get the current IP address for the active network interface
 # # This script handles both WiFi and wired connections
 
+# Update for Mac Sequoia 15.6+, ipconfig getsummary dont show NetworkID and SSID anymore
+# https://github.com/noperator/wifi-unredactor
+
 update_ip() {
   # Get the active network interface (the one with default route)
   ACTIVE_INTERFACE=$(route -n get default | grep interface | awk '{print $2}')
@@ -12,21 +15,22 @@ update_ip() {
     #   IP_ADDRESS="Disconnected"
   else
   # Try to get the WiFi SSID using ipconfig getsummary
-    NETWORK_NAME=$(ipconfig getsummary "$ACTIVE_INTERFACE" | grep ' SSID ' | awk -F' : ' '{print $2}' | tr -d '"')
-  # IP_ADDRESS=$(ipconfig getifaddr "$ACTIVE_INTERFACE")
+    #NETWORK_NAME=$(ipconfig getsummary "$ACTIVE_INTERFACE" | grep ' SSID ' | awk -F' : ' '{print $2}' | tr -d '"')
+    NETWORK_NAME=$(~/Applications/wifi-unredactor.app/Contents/MacOS/wifi-unredactor | grep '"ssid" :' | awk -F' : ' '{print $2}' | tr -d '",')
+    IP_ADDRESS=$(ipconfig getifaddr "$ACTIVE_INTERFACE")
 
     # If no SSID found, assume wired connection
     if [ -z "$NETWORK_NAME" ]; then
       NETWORK_NAME="Wired"
     fi
-  #   if [ -z "$IP_ADDRESS" ]; then
-  #     IP_ADDRESS="No IP"
-  #   fi
+    if [ -z "$IP_ADDRESS" ]; then
+      IP_ADDRESS="No IP"
+    fi
   fi
 
   # Update the sketchybar item
-  sketchybar --set "$NAME" icon="􀙇" label="$NETWORK_NAME"
-    # sketchybar --set "$NAME" icon="􀙇" label="$IP_ADDRESS"
+  sketchybar --set "$NAME" icon="􀙇" label="$NETWORK_NAME | $IP_ADDRESS"
+  #sketchybar --set "$NAME" icon="􀙇" label="$IP_ADDRESS"
 }
 
 # Main logic for sketchybar events
